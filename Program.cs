d1ac475ef153db1b40c5e7e5d3ce66159a8cd0f2
endrik2700 <42510329+endrik2700@users.noreply.github.com>
2019-09-20 16:38:09 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 
 namespace IT_projects__dev_phase
@@ -6,36 +6,87 @@ namespace IT_projects__dev_phase
     class Program
     {
         static void Main(string[] args)
-        {  
+        {
             string name;
             int age;
-            //double bodyMass;
+            //double bodyMass  = calculateBodyMass();
+            //int intBodyMass = int.Parse(calculateBodyMass().ToString()); 
+             
             //double weight;
             //double length;
             Console.WriteLine("What's your name?");
-            name =Console.ReadLine();
+            name = Console.ReadLine();
             Console.WriteLine("Hello " + name + " do you know c# sucks? Anyways, what's your age- type it below please?");
             age = int.Parse(Console.ReadLine());
             if (age >= 20 && age < 65)
             {
-                
-                Console.WriteLine(calculateBodyMass());
+
+                Console.WriteLine(CalculateBodyMass());
                 Console.ReadLine();
+                Console.WriteLine(BodyMassSuggestions(CalculateBodyMass()));
+                Console.ReadLine();
+                
             }
             else
             {
                 Console.WriteLine("You can not get right results as your age is not between 20-60 years. Therefore there will be no calculations, thank you!");
             }
-
+            
 
         }
-        public static double calculateBodyMass () {
-            Console.WriteLine ("What's your bodyweight in kg-s  -(use a point insetad of a comma) ?");
-         double bodyWeight = double.Parse(Console.ReadLine());
+          public static string BodyMassSuggestions (double num) {
+            string suggestion;
+
+            if (CalculateBodyMass()<16) {
+
+                suggestion = "You are in severe underweight)";
+                return suggestion;
+            }
+
+            else if (CalculateBodyMass() >= 16 && CalculateBodyMass()<18.6) {
+                suggestion = "You are in underweight)";
+                return suggestion;
+            }
+            else if (CalculateBodyMass() >= 18.6 && CalculateBodyMass() <=25)
+            {
+                suggestion = "You are in normal weight)";
+                return suggestion;
+            }
+            else if (CalculateBodyMass() >= 25.1 && CalculateBodyMass() <= 30)
+            {
+                suggestion = "You are in overweight)";
+                return suggestion;
+            }
+
+            else if (CalculateBodyMass() >= 30.1 && CalculateBodyMass() <=35)
+            {
+                suggestion = "You are in obesity)";
+                return suggestion;
+            }
+            else if (CalculateBodyMass() >= 35 && CalculateBodyMass() <= 40)
+            {
+                suggestion = "You are in high obesity)";
+                return suggestion;
+            }
+            else
+            {
+                suggestion = "You are in dangerous to healt level of  obesity)";
+                return suggestion;
+            }
+
+            //return suggestion;
+        }
+
+
+
+        public static Double CalculateBodyMass()
+        {
+            Console.WriteLine("What's your bodyweight in kg-s  -(use a point insetad of a comma) ?");
+            Double bodyWeight = Double.Parse(Console.ReadLine());
             Console.WriteLine("What's your length in centimeters?");
-         double lengthSquare = Math.Pow (double.Parse(Console.ReadLine()),2);
-            double bodyMass = bodyWeight/lengthSquare;
-            return bodyMass; 
+            Double lengthSquare = Math.Pow(Double.Parse(Console.ReadLine()), 2);
+            Double bodyMass = bodyWeight / lengthSquare;
+            return bodyMass;
         }
     }
 }
